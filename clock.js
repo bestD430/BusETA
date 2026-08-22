7c29f73ba25bb932087e7f530e22f147a4bfb64b
bestD430 <bestoffer9000@gmail.com>
2026-08-22 11:05:08 +0800

    Refactor clock.js for cleaner code
    Removed unnecessary line breaks and cleaned up formatting.

--- clock.js
+++ clock.js
@@ -1,27 +1,14 @@
 function updateClock(){
-2
  
-3
 const now = new Date();
-4
  
-5
 document.getElementById("clock").innerHTML =
-6
 now.toLocaleTimeString("zh-HK");
-7
  
-8
 document.getElementById("date").innerHTML =
-9
 now.toLocaleDateString("zh-HK");
-10
 }
-11
  
-12
 setInterval(updateClock,1000);
-13
  
-14
 updateClock();
