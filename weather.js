ae522d12a9fe0ec83b484fe5cb5499b6feca3d80
bestD430 <bestoffer9000@gmail.com>
2026-08-22 11:09:47 +0800

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,17 +1,9 @@
 document.getElementById("tungchung").innerHTML = `
-2
 今日 31°C ☀️<br>
-3
 明日 29°C 🌦
-4
 `;
-5
  
-6
 document.getElementById("tst").innerHTML = `
-7
 今日 32°C ⛅<br>
-8
 明日 30°C 🌧
-9
 `;
