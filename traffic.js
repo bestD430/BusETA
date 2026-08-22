efaa6b0a977034e86e724e91c17a4c34cba9f758
bestD430 <bestoffer9000@gmail.com>
2026-08-22 09:24:55 +0800

    Update traffic.js

--- traffic.js
+++ traffic.js
@@ -1,7 +1,53 @@
-document.getElementById("traffic-msg").innerHTML = `
+let busTime = 42;
 2
-🚍 巴士：42分鐘<br>
+let taxiTime = 28;
 3
-🚕 的士：28分鐘
+ 
 4
+function getTrafficIcon(minutes){
+5
+ 
+6
+if(minutes <= 45){
+7
+return "🟢 暢順";
+8
+}
+9
+ 
+10
+if(minutes <= 60){
+11
+return "🟡 車多";
+12
+}
+13
+ 
+14
+return "🔴 擠塞";
+15
+}
+16
+ 
+17
+document.getElementById("traffic-msg").innerHTML = `
+18
+ 
+19
+🚍 巴士：${busTime}分鐘<br>
+20
+${getTrafficIcon(busTime)}
+21
+ 
+22
+<br><br>
+23
+ 
+24
+🚕 的士：${taxiTime}分鐘<br>
+25
+${getTrafficIcon(taxiTime)}
+26
+ 
+27
 `;
