565d1af4ce3a8da14ba0678e998639a27a1cfb21
bestD430 <bestoffer9000@gmail.com>
2026-08-23 08:28:21 +0800

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,14 +1,13 @@
-function updateClock(){
- 
-const now = new Date();
- 
-document.getElementById("clock").innerHTML =
-now.toLocaleTimeString("zh-HK");
- 
-document.getElementById("date").innerHTML =
-now.toLocaleDateString("zh-HK");
+function updateClock() {
+  const now = new Date();
+
+  document.getElementById("clock").innerHTML =
+    now.toLocaleTimeString("zh-HK");
+
+  document.getElementById("date").innerHTML =
+    now.toLocaleDateString("zh-HK");
 }
- 
-setInterval(updateClock,1000);
- 
+
+setInterval(updateClock, 1000);
+
 updateClock();
